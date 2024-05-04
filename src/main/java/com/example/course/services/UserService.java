b08b7535e9d721451012cabdaff07dac0871c2f8
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-05-04 01:10:47 -0300

    User insert

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -3,10 +3,7 @@ package com.example.course.services;
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,9 +18,12 @@ public class UserService {
     return repository.findAll();
   }
 
-  @GetMapping
   public User findById(Long id) {
     Optional<User> obj = repository.findById(id);
     return  obj.get();
+  }
+
+  public User insert(User obj) {
+    return repository.save(obj);
   }
 }
