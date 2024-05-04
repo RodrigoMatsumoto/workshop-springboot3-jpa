4bf25d46ebe90923a4f2202c05b9d412ce700c4b
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-05-04 01:54:43 -0300

    User delete

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -5,6 +5,7 @@ import com.example.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.plaf.PanelUI;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,9 @@ public class UserService {
 
   public User insert(User obj) {
     return repository.save(obj);
+  }
+
+  public void delete(Long id) {
+    repository.deleteById(id);
   }
 }
